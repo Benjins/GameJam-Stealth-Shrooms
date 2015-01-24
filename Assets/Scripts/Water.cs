e6cd70326cf1c22db71293d69bd899bc57c393b0
Benji Smith <smith.benj@husky.neu.edu>
2015-01-24 17:37:55 -0500

    Added indoor scene with parents.

---
+++ Assets/Scripts/Water.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Water : MonoBehaviour {
+
+	public bool held = false;
+
+	GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp(KeyCode.U)){
+			held = !held;
+		}
+		if(held && Input.GetKeyUp(KeyCode.F)){
+			//Drink the water
+		}
+
+		if(held){
+			transform.position = player.transform.position + player.transform.forward;
+		}
+	}
+}
