a82cacb182df16d62b2df51c0dff776eea4eb160
Benji Smith <smith.benj@husky.neu.edu>
2015-01-24 09:03:37 -0500

    Basic pathing test setup.

---
+++ Assets/Scripts/Dijkstra.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Dijkstra {
+
+	public static Stack<Vector3> DijkstraAlg(PathingNode start, PathingNode end, PathingNode[] graph){
+		Dictionary<PathingNode,float> dist = new Dictionary<PathingNode, float>();
+		Dictionary<PathingNode,PathingNode> prev = new Dictionary<PathingNode, PathingNode>();
+		List<PathingNode> Q = new List<PathingNode>();
+
+		dist[start] = 0;
+		prev[start] = null;
+
+		foreach(PathingNode node in graph){
+			if(node != start){
+				dist[node] = Mathf.Infinity;
+				prev[node] = null;
+			}
+
+			Q.Add(node);
+		}
+
+		while(Q.Count > 0){
+			PathingNode u = null;
+			float minDist = Mathf.Infinity;
+			foreach(PathingNode node in Q){
+				if(dist[node] < minDist){
+					minDist = dist[node];
+					u = node;
+				}
+			}
+
+			if(u == end){
+				Stack<Vector3> path = new Stack<Vector3>();
+				PathingNode curr = end;
+				while(curr != null){
+					path.Push(curr.transform.position);
+					curr = prev[curr];
+				}
+
+				return path;
+			}
+
+			Q.Remove(u);
+
+			foreach(PathingNode neigh in u.neighbors){
+				float alt = dist[u] + (u.transform.position - neigh.transform.position).magnitude;
+				if(alt < dist[neigh]){
+					dist[neigh] = alt;
+					prev[neigh] = u;
+				}
+			}
+		}
+
+		return null;
+	}
+}
---
+++ Assets/Scripts/ParentPathing.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ParentPathing : MonoBehaviour {
+	public float speed = 3.0f;
+	public float rotationSpeed = 60.0f;
+	Vector3 immediateTarget;
+
+	Stack<Vector3> pathingPoints;
+
+	PathingNode pathingGoal;
+	PathingNode currentNode;
+	PathingNode[] nodes;
+
+	// Use this for initialization
+	void Start () {
+		nodes = FindObjectsOfType<PathingNode>();
+		RecalculateCurrentNode();
+		GetRandomGoalNode();
+		RecalculatePath();
+		immediateTarget = pathingPoints.Pop();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 goalDirection = immediateTarget - transform.position;
+		Quaternion goalRotation = Quaternion.LookRotation(goalDirection);
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, goalRotation, rotationSpeed);
+		transform.position += transform.forward * Time.deltaTime *speed;
+	}
+
+	public void RecalculateCurrentNode(){
+		float closestDistSqr = Mathf.Infinity;
+		PathingNode closestNode = null;
+		foreach(PathingNode node in nodes){
+			float distSqr = (transform.position - node.transform.position).sqrMagnitude;
+			if(distSqr < closestDistSqr){
+				closestNode = node;
+				closestDistSqr = distSqr;
+			}
+		}
+
+		currentNode = closestNode;
+	}
+
+	public void GetRandomGoalNode(){
+		int index = Random.Range(0,nodes.Length);
+		pathingGoal = nodes[index];
+		Debug.Log("Random goal node: " + index);
+	}
+
+	public void RecalculatePath(){
+		pathingPoints = Dijkstra.DijkstraAlg(currentNode,pathingGoal,nodes);
+		immediateTarget = pathingPoints.Pop();
+	}
+
+	void OnTriggerEnter(Collider col){
+		Debug.Log("OnTriggerEnter()");
+		if((transform.position - immediateTarget).magnitude < 2){
+			Debug.Log("OnTriggerEnter(): close enough to target");
+			PathingNode node = col.gameObject.GetComponent<PathingNode>();
+			if(node != null){
+				Debug.Log("OnTriggerEnter with node.");
+				RecalculateCurrentNode();
+				if(node == pathingGoal){
+					Debug.Log("Reached goal");
+					GetRandomGoalNode();
+					RecalculatePath();
+					immediateTarget = pathingPoints.Pop();
+				}
+				else{
+					Debug.Log("Reached non-goal node");
+					RecalculateCurrentNode();
+					immediateTarget = pathingPoints.Pop();
+				}
+			}
+		}
+		else{
+			Debug.Log("transform.position: " + transform.position + "  immTarget: " + immediateTarget);
+		}
+	}
+}
