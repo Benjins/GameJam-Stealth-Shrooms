10935d7c6dadc9b4e4432f413ce1624936792152
Benji Smith <smith.benj@husky.neu.edu>
2015-01-24 18:09:25 -0500

    Added MusicManager with music levels.

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicManager : MonoBehaviour {
+
+	public static MusicManager instance;
+
+	public AudioClip[] songs;
+	int currentSong = 0;
+
+	void Awake(){
+		if(instance == null){
+			instance = this;
+		}
+
+		if(instance != this){
+			Destroy(gameObject);
+		}
+	}
+
+	public void IncreaseTrippy(){
+		currentSong = Mathf.Clamp(currentSong + 1, 0, songs.Length-1);
+		audio.clip = songs[currentSong];
+		audio.Play();
+	}
+
+	public void DecreaseTrippy(){
+		currentSong = Mathf.Clamp(currentSong - 1, 0, songs.Length-1);
+		audio.clip = songs[currentSong];
+		audio.Play();
+	}
+
+	// Use this for initialization
+	void Start () {	
+		audio.loop = true;
+		audio.clip = songs[currentSong];
+		audio.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp(KeyCode.B)){
+			IncreaseTrippy();
+		}
+		if(Input.GetKeyUp(KeyCode.N)){
+			DecreaseTrippy();
+		}
+	}
+}
