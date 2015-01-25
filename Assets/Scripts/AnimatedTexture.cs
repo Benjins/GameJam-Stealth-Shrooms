90904d3a81009492b471b09ccffe4b1197c3c9a1
Benji Smith <smith.benj@husky.neu.edu>
2015-01-24 19:46:00 -0500

    Animted Texture script.

---
+++ Assets/Scripts/AnimatedTexture.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatedTexture : MonoBehaviour {
+
+	public Texture[] frames;
+	public int frameRate = 12;
+
+	float elapsedTime = 0.0f;
+	int frame = 0;
+
+	// Use this for initialization
+	void Start () {
+		renderer.material.mainTexture = frames[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int newFrame = Mathf.FloorToInt(elapsedTime * frameRate);
+		newFrame = newFrame % frames.Length;
+		if(frame != newFrame){
+			renderer.material.mainTexture = frames[frame];
+			frame = newFrame;
+		}
+
+		elapsedTime += Time.deltaTime;
+	}
+}
