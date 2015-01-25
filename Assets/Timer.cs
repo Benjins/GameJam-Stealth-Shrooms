0cd294770ef491dcbd85a7562948ac7004cfe63c
Eric Lange <langee@WITPB03BNJE.wit.private>
2015-01-24 20:14:37 -0500

    Added timer

---
+++ Assets/Timer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	public float timeLeft = 300f;
+	public int min, sec;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeLeft -= Time.deltaTime;
+		min = (int)(timeLeft / 60);
+		sec = (int)(timeLeft - (min * 60));
+	}
+
+	void OnGUI(){
+		GUI.contentColor = Color.red;
+		GUI.skin.label.fontSize = 20;
+		int x = (int)(Screen.width * .87);
+		int y = (int)(Screen.height * .84);
+		print ("width: " + Screen.width);
+		print ("height: " + Screen.height);
+		GUI.Label (new Rect (x, y, 100, 30), min.ToString() +" : " + sec.ToString());
+
+	}
+}
