ce07cb9c76f5f5e7085716547b6c7ecb2f147c62
Jason Fan <Jason_Fan@Jasons-MacBook-Pro.local>
2015-01-24 21:44:33 -0500

    Trip Objects Updated => Object Trans module

---
+++ Assets/Scripts/ObjectTrans.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectTrans : MonoBehaviour {
+
+	public float x, y, z;
+	
+	public bool Pulsing = false;
+	public float Pulse_rate;
+	
+	void Pulse() {
+		float var = Mathf.Sin (Time.time) * Pulse_rate/100;
+		transform.localScale += new Vector3 (var, var, var);
+	}
+	
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3 (x, y, z));
+		if (Pulsing == true) {
+			Pulse ();
+		}
+	}
+}
