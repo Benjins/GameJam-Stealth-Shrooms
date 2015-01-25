beba1ca2d2bdb8ffb08bf68c7fb520ea784be895
Benji Smith <smith.benj@husky.neu.edu>
2015-01-25 12:37:46 -0500

    Added ui scaler and ui display prefab.

---
+++ Assets/ScaleUIWithScreen.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScaleUIWithScreen : MonoBehaviour {
+
+	float originalScale;
+	CanvasScaler canvasScaler; 
+
+	// Use this for initialization
+	void Start () {
+		canvasScaler = GetComponent<CanvasScaler>();
+		originalScale = canvasScaler.scaleFactor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Screen.width <= 0){
+			canvasScaler.scaleFactor = 1;
+		}
+		else{
+			canvasScaler.scaleFactor = originalScale * (((float)Screen.width)/1920);
+		}
+
+	}
+}
