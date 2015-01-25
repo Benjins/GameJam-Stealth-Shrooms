69bb34dfd132e034a8083642d5f3201d7aa2d315
Benji Smith <smith.benj@husky.neu.edu>
2015-01-25 09:41:16 -0500

    Added level flow elements.

---
+++ Assets/WinBox.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinBox : MonoBehaviour {
+
+	BoxCollider boxCol;
+	GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+		boxCol = GetComponent<BoxCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(boxCol.bounds.Contains(player.transform.position)){
+			Debug.Log("You Win!");
+		}
+	}
+}
